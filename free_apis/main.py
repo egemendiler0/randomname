c0d3356995a02c1ea207a2e0ed8cbff089a942d7
EgemenDiler <egemen.diler@std.yeditepe.edu.tr>
2025-03-23 12:50:28 +0300

    comments added

--- free_apis/main.py
+++ free_apis/main.py
@@ -7,6 +7,3 @@ app = FastAPI()
 app.include_router(random_router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- free_apis/router/random_name_router.py
+++ free_apis/router/random_name_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 import requests
+import re
 from utils import configs as config
 
 router = APIRouter(prefix=config.ROUTER_PREFIX, tags=[config.TAG_WEATHER])
@@ -12,10 +13,10 @@ class RandomNameResponse(BaseModel):
 
 @router.get("", response_model=RandomNameResponse)
 def get_random_name():
-    api_url = "https://randomuser.me/api/"
+    api_url = "https://randomuser.me/api/"   # env den al
     response = requests.get(api_url)
 
-    if response.status_code != 200:
+    if response.status_code != 200:  # constants a al 200 kodu HTTPResponseStatusCode
         raise HTTPException(status_code=500, detail="Random name API request failed")
 
     data = response.json()
@@ -25,5 +26,8 @@ def get_random_name():
     name_info = data["results"][0]["name"]
     full_name = f"{name_info['first']} {name_info['last']}"
 
-    return RandomNameResponse(random_name=full_name)
+    # Özel karakter kontrolü (sadece harf, boşluk, - ve ' izin verilir)
+    if not re.match(r"^[a-zA-Z '-]+$", full_name):
+        raise HTTPException(status_code=400, detail="Generated name contains invalid characters")
 
+    return RandomNameResponse(random_name=full_name)
